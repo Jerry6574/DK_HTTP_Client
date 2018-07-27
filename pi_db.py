6ecaf3e944f5fc1bee8514e3acafead6c47fc97b
jerryw <wu@assmann-wsw.com>
2018-07-27 15:57:05 -0700

    moved concat_spg to pi_db

--- pi_db.py
+++ pi_db.py
@@ -1,9 +1,20 @@
-import sqlite3
+import time
+from utils import concat_spg
 
 
-class PIDB:
+def main():
+    # abs_spg_dirs = get_abs_spg_dirs(r"C:\Users\jerryw\Desktop\AWSW and Competitor Product on DK 2018-06-29")
+    # concat_xl_dir = r"C:\Users\jerryw\Desktop\07-20-18 Product Index Concat XL"
+    #
+    # mp_func(concat_spg_csv, abs_spg_dirs, has_return=False, sec_arg=concat_xl_dir, mode='process')
 
-    def __init__(self, db_path):
-        self.db_path = db_path
+    df_concat_intersect_cols = concat_spg(r"C:\Users\jerryw\Desktop\product_index_20180703-20 concat xl")
+    df_concat_intersect_cols = df_concat_intersect_cols[df_concat_intersect_cols['Manufacturer'] == 'Assmann WSW Components']
+    df_concat_intersect_cols.to_excel(r"C:\Users\jerryw\Desktop\AWSW on DK.xlsx")
 
-    def create_tables(self):
+
+if __name__ == '__main__':
+    t1 = time.time()
+    main()
+    t2 = time.time() - t1
+    print("Took", t2, "seconds")
--- utils.py
+++ utils.py
@@ -133,7 +133,7 @@ def intersect(n_list):
     return result
 
 
-def extract_cols(file_path, mode='price'):
+def extract_cols(file_path, mode='intersect'):
     intersect_cols = ['Digi-Key Part Number', 'Manufacturer', 'Manufacturer Part Number', 'Quantity Available',
                       'Unit Price (USD)', 'Datasheets', 'Series', 'Part Status', 'Minimum Quantity',
                       'Description', 'Image', '@ qty', 'Factory Stock']
@@ -162,18 +162,4 @@ def concat_spg(read_dir):
     return df_concat_intersect_cols
 
 
-# def main():
-#     # abs_spg_dirs = get_abs_spg_dirs(r"C:\Users\jerryw\Desktop\AWSW and Competitor Product on DK 2018-06-29")
-#     # concat_xl_dir = r"C:\Users\jerryw\Desktop\07-20-18 Product Index Concat XL"
-#     #
-#     # mp_func(concat_spg_csv, abs_spg_dirs, has_return=False, sec_arg=concat_xl_dir, mode='process')
-#
-#     df_concat_intersect_cols = concat_spg(r"C:\Users\jerryw\Desktop\product_index_20180703-20 concat xl")
-#     df_concat_intersect_cols.to_csv(r"C:\Users\jerryw\Desktop\df_concat_intersect_cols.csv")
-#
-#
-# if __name__ == '__main__':
-#     t1 = time.time()
-#     main()
-#     t2 = time.time() - t1
-#     print("Took", t2, "seconds")
+
